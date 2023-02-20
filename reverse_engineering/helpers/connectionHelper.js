80a37f0cc8292758b5aeed29a659fa9f910bb9bb
Vitalii Bedletskyi <vitalii.bedletskyi@binary-studio.com>
2023-02-20 17:41:40 +0200

    incapsulate work with aws-sdk to connection helper

---
+++ reverse_engineering/helpers/connectionHelper.js
@@ -0,0 +1,116 @@
+const aws = require('aws-sdk');
+const fs = require('fs');
+const https = require('https');
+const {mapTableData} = require("./tablePropertiesHelper");
+
+let connection;
+
+const readCertificateFile = path => {
+    if (!path) {
+        return Promise.resolve('');
+    }
+
+    return new Promise(resolve => {
+        fs.readFile(path, 'utf8', (err, data) => {
+            if (err) {
+                resolve('');
+            }
+            resolve(data);
+        });
+    });
+};
+const getSslOptions = async connectionInfo => {
+    switch (connectionInfo.sslType) {
+        case 'Server validation': {
+            const certAuthority = await readCertificateFile(connectionInfo.certAuthorityPath);
+            return {
+                ssl: true,
+                ca: [certAuthority],
+            };
+        }
+        case 'Server and client validation': {
+            const certAuthority = await readCertificateFile(connectionInfo.certAuthorityPath);
+            const key = await readCertificateFile(connectionInfo.clientPrivateKey);
+            const cert = await readCertificateFile(connectionInfo.clientCert);
+            return {
+                ssl: true,
+                ca: [certAuthority],
+                key: [key],
+                cert: [cert],
+                passphrase: connectionInfo.clientKeyPassword,
+            };
+        }
+        default:
+            return { ssl: false };
+    }
+};
+
+const createConnection = async (connectionInfo) => {
+    const { accessKeyId, secretAccessKey, region, sessionToken } = connectionInfo;
+    const sslOptions = await getSslOptions(connectionInfo);
+    const httpOptions = sslOptions.ssl ? {
+        httpOptions: {
+            agent: new https.Agent({
+                rejectUnauthorized: true,
+                ...sslOptions
+            })},
+        ...sslOptions
+    } : {};
+
+    aws.config.update({ accessKeyId, secretAccessKey, region, sessionToken, ...httpOptions });
+
+    return new aws.Glue();
+};
+const connect = async (connectionInfo) => {
+    if (connection) {
+        return connection;
+    }
+
+    connection = await createConnection(connectionInfo);
+
+    return connection;
+};
+
+const close = () => {
+    if (connection) {
+        connection = null;
+    }
+};
+
+const createInstance = (connection, _) => {
+    const getDatabases = async () => {
+        const dbsData = await connection.getDatabases().promise();
+        return dbsData.DatabaseList;
+    };
+
+    const getDatabaseDescription = async (dbName) => {
+        const db = await connection.getDatabase({ Name: dbName }).promise();
+        return db.Database.Description;
+    };
+
+    const getTables = async (dbName) => {
+        const dbCollectionsData = await connection.getTables({ DatabaseName: dbName }).promise();
+        return dbCollectionsData.TableList.map(({ Name }) => Name);
+    };
+
+    const getTable = async (dbName, tableName) => {
+        const rawTableData = await connection
+            .getTable({ DatabaseName: dbName, Name: tableName })
+            .promise();
+
+        return mapTableData(rawTableData, _);
+    };
+
+    return {
+        getDatabaseDescription,
+        getDatabases,
+        getTables,
+        getTable,
+    };
+}
+
+module.exports = {
+    createInstance,
+    connect,
+    close,
+};
---
+++ reverse_engineering/helpers/tablePropertiesHelper.js
@@ -0,0 +1,88 @@
+const mapSortColumns = (items) => {
+    return items.map(item => ({
+        name: item.Column,
+        type: item.SortOrder === 1 ? 'ascending' : 'descending'
+    }));
+}
+
+const getSerDeLibrary = (data = {}) => {
+    return data.SerializationLibrary;
+}
+
+const mapSerDePaths = (_, data = {}) => {
+    return _.get(data, 'Parameters.paths', '').split(',');
+}
+
+const mapSerDeParameters = (parameters = {}) => {
+    return Object.entries(parameters).reduce((acc, [key, value]) => {
+        if (key !== 'paths') {
+            acc.push({ serDeKey: key, serDeValue: value });
+            return acc;
+        }
+        return acc;
+    }, []);
+}
+
+const getClassification = (parameters = {}) => {
+    if (parameters.classification) {
+        switch (parameters.classification.toLowerCase()) {
+            case 'avro':
+                return 'Avro';
+            case 'csv':
+                return 'CSV';
+            case 'json':
+                return 'JSON';
+            case 'xml':
+                return 'XML';
+            case 'parquet':
+                return 'Parquet';
+            case 'orc':
+                return 'ORC';
+        }
+    }
+    return {};
+}
+
+const mapTableProperties = (parameters = {}) => {
+    return Object.entries(parameters).reduce((acc, [key, value]) => {
+        if (key === 'classification') {
+            return acc;
+        }
+        return acc.concat({
+            tablePropKey: key,
+            tablePropValue: value
+        });
+    }, []);
+}
+
+const mapTableData = (tableData, _) => {
+    const partitionKeys = tableData.Table.PartitionKeys || [];
+
+    return {
+        name: tableData.Table.Name,
+        entityLevelData: {
+            description: tableData.Table.Description,
+            externalTable: tableData.Table.TableType === 'EXTERNAL_TABLE',
+            tableProperties: mapTableProperties(tableData.Table.Parameters),
+            compositePartitionKey: partitionKeys.map(item => item.Name),
+            compositeClusteringKey: tableData.Table.StorageDescriptor.BucketColumns,
+            sortedByKey: mapSortColumns(tableData.Table.StorageDescriptor.SortColumns),
+            compressed: tableData.Table.StorageDescriptor.Compressed,
+            location: tableData.Table.StorageDescriptor.Location,
+            numBuckets: tableData.Table.StorageDescriptor.NumberOfBuckets,
+            StoredAsSubDirectories: tableData.Table.StorageDescriptor.StoredAsSubDirectories,
+            inputFormatClassname: tableData.Table.StorageDescriptor.InputFormat,
+            outputFormatClassname: tableData.Table.StorageDescriptor.OutputFormat,
+            serDeLibrary: getSerDeLibrary(tableData.Table.StorageDescriptor.SerdeInfo),
+            parameterPaths: mapSerDePaths(_, tableData.Table.StorageDescriptor.SerdeInfo),
+            serDeParameters: mapSerDeParameters(tableData.Table.StorageDescriptor.SerdeInfo.Parameters),
+            classification: getClassification(tableData.Table.Parameters),
+        },
+        partitionKeys: tableData.Table.PartitionKeys || [],
+        columns: tableData.Table.StorageDescriptor.Columns.map(({ Type, Name }) => ({ name: Name, type: Type })),
+    };
+}
+
+module.exports = {
+    mapTableData,
+};
